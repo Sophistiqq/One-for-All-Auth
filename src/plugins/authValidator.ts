572a8a2ed15fe55b364a2cfe3bf3ce89d706cf10
Roi Borromeo <roi.for.school@gmail.com>
2026-01-11 21:17:58 +0800

    fix: moved jwt secret into env file for security

--- src/plugins/authValidator.ts
+++ src/plugins/authValidator.ts
@@ -1,12 +1,16 @@
 import { Elysia, t } from "elysia"
 import jwt from "@elysiajs/jwt";
 
+if (!process.env.JWT_SECRET) {
+  throw new Error("JWT_SECRET environment variable is required")
+}
+
 // This is a macro that can be used to validate the auth cookie.
 // Just simply add the `isAuth` property to your route and it will be validated.
 export const validator = new Elysia()
   .use(jwt({
     name: "jwt_token",
-    secret: "Roi",
+    secret: process.env.JWT_SECRET,
     exp: 60 * 60 * 24 * 30,
   }))
   .macro("isAuth", {
@@ -16,7 +20,7 @@ export const validator = new Elysia()
     async resolve({ cookie: { auth_cookie }, status, jwt_token }) {
       const token = await jwt_token.verify(auth_cookie.value as string)
       if (!token) return status(401, "Please login to continue.")
-      
+
       // Parse the token.sub into a number
       const userId = Number(token.sub)
       if (!Number.isInteger(userId)) {
